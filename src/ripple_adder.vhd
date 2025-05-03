9126e9ae522d4031a4e9a73d6f65b925b83830cc
dexter-r-s-james-2027 <193975598+dexter-r-s-james-2027@users.noreply.github.com>
2025-05-02 21:41:42 -0600

    Update ripple_adder.vhd

--- src/ripple_adder.vhd
+++ src/ripple_adder.vhd
@@ -1,43 +1,71 @@
 ----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 05/02/2025 09:23:32 PM
--- Design Name: 
--- Module Name: ripple_adder - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
+-- Implements a 4-bit Ripple-Carry adder from instantiated Full Adders
 ----------------------------------------------------------------------------------
-
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
 entity ripple_adder is
---  Port ( );
+    Port ( A : in STD_LOGIC_VECTOR (3 downto 0);
+           B : in STD_LOGIC_VECTOR (3 downto 0);
+           Cin : in STD_LOGIC;
+           S : out STD_LOGIC_VECTOR (3 downto 0);
+           Cout : out STD_LOGIC);
 end ripple_adder;
 
 architecture Behavioral of ripple_adder is
 
-begin
+    -- Declare components here
+    component full_adder is
+        port (
+            A     : in std_logic;
+            B     : in std_logic;
+            Cin   : in std_logic;
+            S     : out std_logic;
+            Cout  : out std_logic
+            );
+        end component full_adder;
+    
+    -- Declare signals here
+        signal w_carry  : STD_LOGIC_VECTOR(3 downto 0); -- for ripple between adders
 
+begin
+    full_adder_0: full_adder
+    port map(
+        A     => A(0),
+        B     => B(0),
+        Cin   => Cin,   -- Directly to input here
+        S     => S(0),
+        Cout  => w_carry(0)
+    );
+
+    full_adder_1: full_adder
+    port map(
+        A     => A(1),
+        B     => B(1),
+        Cin   => w_carry(0),   -- Directly to input here
+        S     => S(1),
+        Cout  => w_carry(1)
+    );
+    
+    full_adder_2: full_adder
+    port map(
+        A     => A(2),
+        B     => B(2),
+        Cin   => w_carry(1),   -- Directly to input here
+        S     => S(2),
+        Cout  => w_carry(2)
+    );
+    
+    full_adder_3: full_adder
+    port map(
+        A     => A(3),
+        B     => B(3),
+        Cin   => w_carry(2),   -- Directly to input here
+        S     => S(3),
+        Cout  => w_carry(3)
+    );
+    
+    Cout <= w_carry(3);
 
 end Behavioral;
