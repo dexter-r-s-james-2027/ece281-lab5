a93769c13de79a3bd99079d00e58c81230d35851
dexter-r-s-james-2027 <193975598+dexter-r-s-james-2027@users.noreply.github.com>
2025-05-02 21:41:19 -0600

    Update full_adder.vhd

--- src/full_adder.vhd
+++ src/full_adder.vhd
@@ -1,43 +1,22 @@
 ----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 05/02/2025 09:24:49 PM
--- Design Name: 
--- Module Name: full_adder - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
+-- Full Adder
+-- See Digital Design and Computer Architecture, RISC-V Figure 5.3 
 ----------------------------------------------------------------------------------
-
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
 entity full_adder is
---  Port ( );
+    Port ( A : in  STD_LOGIC;
+           B : in  STD_LOGIC;
+           Cin : in  STD_LOGIC;
+           S : out  STD_LOGIC;
+           Cout : out  STD_LOGIC);
 end full_adder;
 
 architecture Behavioral of full_adder is
-
 begin
 
-
+        S <= A XOR B XOR Cin;
+        Cout <= (A AND B) OR (B AND Cin) OR (A AND Cin);
+        
 end Behavioral;
